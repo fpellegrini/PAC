c1e9ce95f1bbd76d30ff7c05a18f856ec2b1af1b
fpellegrini <franziskapellegrini@web.de>
2022-03-17 15:36:28 +0100

    different normalization and bug fix for multiple interactions

--- fp_pac_signal.m
+++ fp_pac_signal.m
@@ -28,18 +28,14 @@ coupling_snr = 0.6;
 %%
 
 % filters for band and highpass
-[bband_low, aband_low] = butter(5, low/fs*2);
-[bband_high, aband_high] = butter(5, high/fs*2);
-[bhigh, ahigh] = butter(5, 1/fs*2, 'high');
-aband_all = poly([roots(aband_low);roots(aband_high)]);
-bband_all = conv(bband_low,bband_high);
+[bband_low, aband_low] = butter(5, low/(fs/2));
+[bband_high, aband_high] = butter(5, high/(fs/2));
+[bhigh, ahigh] = butter(5, 1/(fs/2), 'high');
   
 filt.aband_low = aband_low;
 filt.bband_low = bband_low;
 filt.aband_high = aband_high;
 filt.bband_high = bband_high;
-filt.aband_all = aband_all; 
-filt.bband_all = bband_all;
 filt.ahigh = ahigh;
 filt.bhigh = bhigh;
 filt.band_inds_low = band_inds_low;
@@ -71,6 +67,7 @@ sig_ind = [];
 for ii = 1:params.iReg
     if params.case==1 %univariate 
         sig_ind = [sig_ind; (iroi_phase.*params.iReg)-(ii-1)];
+        iroi_amplt = iroi_phase; 
     elseif params.case==2 %bivariate 
         sig_ind = [sig_ind; (iroi_phase.*params.iReg)-(ii-1), (iroi_amplt.*params.iReg)-(ii-1)];
     end
@@ -80,11 +77,15 @@ noise_ind = setdiff(1:params.iReg*D.nroi,sig_ind(:));
 
 %% generate signal low and high 
 
-xl = randn(N, 1);
-xl = filtfilt(bband_low, aband_low, xl);
+xl = randn(N,  params.iInt*params.iReg);
+for ii = 1:  params.iInt*params.iReg
+    xl(:,ii) = filtfilt(bband_low, aband_low, xl(:,ii));
+end
 
-xh = randn(N, 1);
-xh = filtfilt(bband_high, aband_high, xh);
+xh = randn(N,  params.iInt*params.iReg);
+for ii = 1: params.iInt*params.iReg
+    xh(:,ii) = filtfilt(bband_high, aband_high, xh(:,ii));
+end
 
 xlh = hilbert(xl);
 xlphase = angle(xlh);
@@ -94,11 +95,11 @@ xhphase = angle(xhh);
 
 xh = real((1-cos(xlphase)).*exp(1i*xhphase));
 
-xh = xh./norm(xh,'fro');
-xl = xl./norm(xl,'fro');
-
 
 %% normalize low and high freq signal to 1/f shape 
+% 
+xh = xh./norm(xh,'fro');
+xl = xl./norm(xl,'fro');
 
 xl = fp_pinknorm(xl);
 xh = fp_pinknorm(xh);
@@ -116,13 +117,12 @@ if params.case==1 %univariate case
 elseif params.case==2 %bivariate case 
     %one region contains low signal, the other the modulated high signal 
     s1 = cat(2,xl,xh);
+    s1 = s1./norm(s1(:),'fro');
 end
 
 % add pink background noise
-backg = mkpinknoise(N, params.iInt*params.iReg*size(s1,2), 1);
-backgf = filtfilt(bband_all, aband_all, backg);
-% normalization is done w.r.t. interacting bands
-backg = backg / norm(backgf, 'fro');
+backg = mkpinknoise(N, size(s1,2), 1);
+backg = backg ./ norm(backg, 'fro');
 
 %combine signal and background noise 
 signal_sources = coupling_snr*s1 + (1-coupling_snr)*backg;
@@ -130,7 +130,7 @@ signal_sources = coupling_snr*s1 + (1-coupling_snr)*backg;
 %% non-interacting sources
 
 %activity at all voxels but the seed and target voxels
-noise_sources = mkpinknoise(N, params.iReg*D.nroi-(params.iReg*params.iInt*size(s1,2)), 1);
+noise_sources = mkpinknoise(N, params.iReg*D.nroi-size(s1,2), 1);
 
 %% leadfield for forward model
 
@@ -151,19 +151,16 @@ L_noise = L_mix(:,noise_ind);
 
 %signal on sensor level 
 sig = L_sig * signal_sources';
-sig_f = (filtfilt(bband_all, aband_all, sig'))'; %%%%%%%%necessary or is this destroying again 1/f signal pattern? 
-sig = sig ./ norm(sig_f, 'fro'); 
+sig = sig ./ norm(sig(:), 'fro');
 
 %brain noise on sensor level 
 try
     brain_noise = L_noise * noise_sources';
-    brain_noise_f = (filtfilt(bband_all, aband_all, brain_noise'))';
-    brain_noise = brain_noise ./ norm(brain_noise_f, 'fro');
+    brain_noise = brain_noise ./ norm(brain_noise(:), 'fro');
 catch
     error('Something went wrong with seed or target selection.')
 end
 
-%white noise on sensor level (sensor noise) 
+%white noise on sensor level (sensor noise)
 sensor_noise = randn(size(sig));
-sensor_noise_f = (filtfilt(bband_all, aband_all, sensor_noise'))';
-sensor_noise = sensor_noise ./ norm(sensor_noise_f, 'fro');
+sensor_noise = sensor_noise ./ norm(sensor_noise(:), 'fro');
