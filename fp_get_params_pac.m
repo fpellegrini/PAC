e2c062134e08eba6eb54d02397b273a34c9377e3
fpellegrini <franziskapellegrini@web.de>
2022-03-21 13:06:55 +0100

    ip 3

--- fp_get_params_pac.m
+++ fp_get_params_pac.m
@@ -54,5 +54,16 @@ elseif ip == 5
     params.ifilt = 'l';
     params.pips = 1;
     params.nshuf = 50;
+
+elseif ip == 2
+    params.case = 2; %bivariate
+    params. iInt = 2;
+    params.isnr=0.3;
+    
+    params.iReg=1;
+    params.iss = 0.5;
+    params.ifilt = 'l';
+    params.pips = 1;
+    params.nshuf = 50;
     
 end
