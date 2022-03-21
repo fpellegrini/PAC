7585055bb0f3fd3c3e33b897d5a0566e64df938e
fpellegrini <franziskapellegrini@web.de>
2022-03-21 17:04:30 +0100

    ip 6 to 8

--- fp_get_params_pac.m
+++ fp_get_params_pac.m
@@ -2,7 +2,7 @@ function params = fp_get_params_pac(ip)
 
 if ip == 1
     params.case = 2; %bivariate
-    params. iInt = 1;
+    params.iInt = 1;
     params.isnr=0.3;
     
     params.iReg=1;
@@ -13,7 +13,7 @@ if ip == 1
     
 elseif ip == 2
     params.case = 2; %bivariate
-    params. iInt = 2;
+    params.iInt = 2;
     params.isnr=0.7;
     
     params.iReg=1;
@@ -24,7 +24,7 @@ elseif ip == 2
     
 elseif ip == 3
     params.case = 1; %univariate
-    params. iInt = 1;
+    params.iInt = 1;
     params.isnr=0.3;
     
     params.iReg=1;
@@ -35,7 +35,7 @@ elseif ip == 3
     
 elseif ip == 4
     params.case = 1; %univariate
-    params. iInt = 2;
+    params.iInt = 2;
     params.isnr=0.7;
     
     params.iReg=1;
@@ -46,7 +46,7 @@ elseif ip == 4
     
 elseif ip == 5
     params.case = 1; %univariate
-    params. iInt = 2;
+    params.iInt = 2;
     params.isnr=0.3;
     
     params.iReg=1;
@@ -55,10 +55,33 @@ elseif ip == 5
     params.pips = 1;
     params.nshuf = 50;
 
-elseif ip == 2
+elseif ip == 6
     params.case = 2; %bivariate
-    params. iInt = 2;
+    params.iInt = 2;
     params.isnr=0.3;
+    
+    params.iReg=1;
+    params.iss = 0.5;
+    params.ifilt = 'l';
+    params.pips = 1;
+    params.nshuf = 50;
+    
+elseif ip == 7
+    params.case = 2; %bivariate
+    params.iInt = 1;
+    params.isnr=0.7;
+    
+    params.iReg=1;
+    params.iss = 0.5;
+    params.ifilt = 'l';
+    params.pips = 1;
+    params.nshuf = 50;
+    
+        
+elseif ip == 8
+    params.case = 1; %univariate
+    params.iInt = 1;
+    params.isnr=0.7;
     
     params.iReg=1;
     params.iss = 0.5;
