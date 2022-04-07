40613746e48eb898faeb1ad9d6feaef10e50acdc
fpellegrini <franziskapellegrini@web.de>
2022-04-07 17:14:26 +0200

    some changes

--- fp_addpath_pac.m
+++ fp_addpath_pac.m
@@ -4,7 +4,13 @@ restoredefaultpath
 rehash toolboxcache
 
 addpath(genpath('/home/bbci/data/haufe/Franziska'))
-addpath(genpath('~/matlab/'))
+addpath(genpath('~/matlab/PAC/'))
+addpath(genpath('~/matlab/matgrid/'))
+addpath(genpath('~/matlab/libs/Daniele_ARMA/'))
+addpath(genpath('~/matlab/libs/haufe/'))
+addpath(genpath('~/matlab/libs/mvgc_v1.0/'))
+addpath(genpath('~/matlab/libs/nolte/'))
+addpath(genpath('~/matlab/libs/pac_lib/'))
 % spm eeg
 % close all 
 cd '/home/bbci/data/haufe/Franziska/data/'
