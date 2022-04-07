7571eb55f5df7a46bddb6a0f8ad1ea6867bfd002
fpellegrini <franziskapellegrini@web.de>
2022-04-07 10:00:46 +0200

    now again variable snrs, iints and pipelines

--- fp_eval_pac_sim.m
+++ fp_eval_pac_sim.m
@@ -11,31 +11,39 @@ rng('shuffle')
 %prevent array jobs to start at exactly the same time
 iit = str2num(getenv('SGE_TASK_ID'))
 
-params = fp_get_params_pac(ip);
+params1 = fp_get_params_pac(ip);
+params = params1;
 
-%create logfile for parallelization
-if params.case == 1
-    logname = sprintf('univar_iInt%d_iReg%d_snr0%d_iss0%d_filt%s_iter%d'...
-        ,params.iInt,params.iReg,params.isnr*10,params.iss*10,params.ifilt,iit);
-elseif params.case == 2
-    logname = sprintf('bivar_iInt%d_iReg%d_snr0%d_iss0%d_filt%s_iter%d'...
-        ,params.iInt,params.iReg,params.isnr*10,params.iss*10,params.ifilt,iit);
+for iInt = params1.iInt
+    params.iInt = iInt;
+    for isnr = params1.isnr
+        params.isnr = isnr;
+        
+        %create logfile for parallelization
+        if params.case == 1
+            logname = sprintf('univar_iInt%d_iReg%d_snr0%d_iss0%d_filt%s_pip%d_iter%d'...
+                ,params.iInt,params.iReg,params.isnr*10,params.iss*10,params.ifilt,params.t,iit);
+        elseif params.case == 2
+            logname = sprintf('bivar_iInt%d_iReg%d_snr0%d_iss0%d_filt%s_pip%d_iter%d'...
+                ,params.iInt,params.iReg,params.isnr*10,params.iss*10,params.ifilt,params.t,iit);
+        end
+        
+        if ~exist(sprintf('%s%s_work',DIRLOG,logname)) & ~exist(sprintf('%s%s_done',DIRLOG,logname))
+            
+            eval(sprintf('!touch %s%s_work',DIRLOG,logname))
+            fprintf('Working on %s. \n',logname)
+            
+            params.iit = iit;
+            params.ip = ip;
+            params.logname = logname;
+            
+            fp_pac_sim(params)
+            
+            eval(sprintf('!mv %s%s_work %s%s_done',DIRLOG,logname,DIRLOG,logname))
+            
+        end  %work_done
+    end
 end
-
-if ~exist(sprintf('%s%s_work',DIRLOG,logname)) & ~exist(sprintf('%s%s_done',DIRLOG,logname))
-    
-    eval(sprintf('!touch %s%s_work',DIRLOG,logname))
-    fprintf('Working on %s. \n',logname)
-    
-    params.iit = iit;
-    params.ip = ip;
-    params.logname = logname;
-    
-    fp_pac_sim(params)
-    
-    eval(sprintf('!mv %s%s_work %s%s_done',DIRLOG,logname,DIRLOG,logname))
-    
-end  %work_done
 
 
 
--- fp_get_params_pac.m
+++ fp_get_params_pac.m
@@ -1,92 +1,100 @@
 function params = fp_get_params_pac(ip)
 
 if ip == 1
+    %default bivariate 
     params.case = 2; %bivariate
     params.iInt = 1;
-    params.isnr=0.3;
+    params.isnr=0.7;
+    params.t = 0; 
     
     params.iReg=1;
-    params.iss = 0.5;
+    params.iss = 0.9;
     params.ifilt = 'l';
     params.pips = 1;
-    params.nshuf = 50;
+    params.nshuf = 500;
     
 elseif ip == 2
+    %vary interactions 
     params.case = 2; %bivariate
-    params.iInt = 2;
+    params.iInt = 2:5;
     params.isnr=0.7;
+    params.t = 0; 
     
     params.iReg=1;
-    params.iss = 0.5;
+    params.iss = 0.9;
     params.ifilt = 'l';
     params.pips = 1;
-    params.nshuf = 50;
+    params.nshuf = 500;
     
 elseif ip == 3
-    params.case = 1; %univariate
+    %vary snr 
+    params.case = 2; %bivariate
     params.iInt = 1;
-    params.isnr=0.3;
+    params.isnr=[0.3 0.5 0.9];
+    params.t = 0; 
     
     params.iReg=1;
-    params.iss = 0.5;
+    params.iss = 0.9;
     params.ifilt = 'l';
     params.pips = 1;
-    params.nshuf = 50;
+    params.nshuf = 500;
     
 elseif ip == 4
-    params.case = 1; %univariate
-    params.iInt = 2;
-    params.isnr=0.7;
-    
-    params.iReg=1;
-    params.iss = 0.5;
-    params.ifilt = 'l';
-    params.pips = 1;
-    params.nshuf = 50;
-    
-elseif ip == 5
-    params.case = 1; %univariate
-    params.iInt = 2;
-    params.isnr=0.3;
-    
-    params.iReg=1;
-    params.iss = 0.5;
-    params.ifilt = 'l';
-    params.pips = 1;
-    params.nshuf = 50;
-
-elseif ip == 6
-    params.case = 2; %bivariate
-    params.iInt = 2;
-    params.isnr=0.3;
-    
-    params.iReg=1;
-    params.iss = 0.5;
-    params.ifilt = 'l';
-    params.pips = 1;
-    params.nshuf = 50;
-    
-elseif ip == 7
+    %truevox pipeline 
     params.case = 2; %bivariate
     params.iInt = 1;
     params.isnr=0.7;
+    params.t = 1; 
     
     params.iReg=1;
-    params.iss = 0.5;
+    params.iss = 0.9;
     params.ifilt = 'l';
     params.pips = 1;
-    params.nshuf = 50;
+    params.nshuf = 500;
     
-        
-elseif ip == 8
-    params.case = 1; %univariate
-    params.iInt = 1;
-    params.isnr=0.7;
-    
-    params.iReg=1;
-    params.iss = 0.5;
-    params.ifilt = 'l';
-    params.pips = 1;
-    params.nshuf = 50;
+% elseif ip == 5
+%     params.case = 1; %univariate
+%     params.iInt = 2;
+%     params.isnr=0.3;
+%     
+%     params.iReg=1;
+%     params.iss = 0.9;
+%     params.ifilt = 'l';
+%     params.pips = 1;
+%     params.nshuf = 500;
+% 
+% elseif ip == 6
+%     params.case = 2; %bivariate
+%     params.iInt = 2;
+%     params.isnr=0.3;
+%     
+%     params.iReg=1;
+%     params.iss = 0.9;
+%     params.ifilt = 'l';
+%     params.pips = 1;
+%     params.nshuf = 500;
+%     
+% elseif ip == 7
+%     params.case = 2; %bivariate
+%     params.iInt = 1;
+%     params.isnr=0.7;
+%     
+%     params.iReg=1;
+%     params.iss = 0.9;
+%     params.ifilt = 'l';
+%     params.pips = 1;
+%     params.nshuf = 500;
+%     
+%         
+% elseif ip == 8
+%     params.case = 1; %univariate
+%     params.iInt = 1;
+%     params.isnr=0.7;
+%     
+%     params.iReg=1;
+%     params.iss = 0.9;
+%     params.ifilt = 'l';
+%     params.pips = 1;
+%     params.nshuf = 500;
     
 end
