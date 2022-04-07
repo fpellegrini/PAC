8aedf7123f758f20ace46942b0b25fe7eac9d554
fpellegrini <franziskapellegrini@web.de>
2022-04-07 10:03:56 +0200

    minor changes

--- pac_init.m
+++ pac_init.m
@@ -1,12 +1,13 @@
-params.ip = 1; 
-params. iInt = 1; 
-params.iReg=1; 
+
+params. iInt = 3; 
 params.case = 2; 
-params.iss = 0.5; 
 params.isnr=0.3; 
+
+params.iReg=1; 
+params.iss = 0.9; 
 params.ifilt = 'l';
 params.pips = 1;
-params.nshuf = 50; 
+params.nshuf = 500; 
 
 % addpath(genpath('~/Dropbox/Franziska/MEG_Project/matlab/libs/fieldtrip-20211001/external/fastica'))
 addpath('~/Dropbox/Franziska/PAC_AAC_estimation/matlab/fp_pac/')
