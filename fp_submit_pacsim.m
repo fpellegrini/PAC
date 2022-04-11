8dee7b8333080d555228b4a54f979b9d5bbc5102
fpellegrini <franziskapellegrini@web.de>
2022-04-11 12:04:03 +0200

    ip 2 and 3

--- fp_submit_pacsim.m
+++ fp_submit_pacsim.m
@@ -3,7 +3,7 @@ fp_addpath_pac
 cd ~/matlab/fp/PAC/
 nit = 100;
 
-for ip = [7:8] %1,4,5,6,7,8 next: 2 and 3 
+for ip = [2:3]
     mgsub({},@fp_eval_pac_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*3)
 end
