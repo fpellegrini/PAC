ba19192ef91c04b81d68bd8984dbe9b3aad27b47
fpellegrini <franziskapellegrini@web.de>
2022-05-17 16:31:34 +0200

    bug fix

--- fp_shuffle_shab.m
+++ fp_shuffle_shab.m
@@ -3,7 +3,7 @@ function signal_shuf = fp_shuffle_shab(W,signal_unmixed)
 [n_sensors, l_epoch, n_trials] = size(signal_unmixed);
 
 for ichan = 1:n_sensors
-    inds = randperm(n_trials,n_trials);
+    inds = randperm(n_trials);
     s_shuf(ichan,:,:) = signal_unmixed(ichan,:,inds);
 end
 
