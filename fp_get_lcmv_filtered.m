e4b7145138008287199e4f9eb8ae6a7fc6f9dfcc
fpellegrini <franziskapellegrini@web.de>
2022-05-24 10:17:29 +0200

    changes: keep snr, second lcmv variant

---
+++ fp_get_lcmv_filtered.m
@@ -0,0 +1,12 @@
+function A = fp_get_lcmv_filtered(signal_sensor,L,filt)
+
+a_all = poly([roots(filt.aband_low);roots(filt.aband_high)]);
+b_all = conv(filt.bband_low,filt.bband_high);
+signal_sensor = filtfilt(b_all, a_all, signal_sensor(:,:)');  
+  
+cCS = cov(signal_sensor);
+reg = 0.05*trace(cCS)/length(cCS);
+Cr = cCS + reg*eye(size(cCS,1));
+
+[~, A] = lcmv(Cr, L, struct('alpha', 0, 'onedim', 0));
+A = permute(A,[1, 3, 2]);
--- fp_pac_sim.m
+++ fp_pac_sim.m
@@ -7,34 +7,68 @@ DIROUT1 = '/home/bbci/data/haufe/Franziska/data/pac_save2/';
 if ~exist(DIROUT1);mkdir(DIROUT1); end
 
 
-%% signal generation
-tic
-% getting atlas, voxel and roi indices; active voxel of each region
-% is aleady selected here
-fprintf('Getting atlas positions... \n')
-D = fp_get_Desikan(params.iReg);
-
-%signal generation
-fprintf('Signal generation... \n')
-[sig,brain_noise,sensor_noise,L,iroi_phase, iroi_amplt,D, fres, n_trials,filt] = ...
-    fp_pac_signal(params,D);
-
-%combine noise sources
-noise = params.iss*brain_noise + (1-params.iss)*sensor_noise;
-noise = noise ./ norm(noise(:),'fro');
-%combine signal and noise
-signal_sensor1 = params.isnr*sig + (1-params.isnr)*noise;
-signal_sensor1 = signal_sensor1 ./ norm(signal_sensor1(:), 'fro');
-
-%high-pass signal
-signal_sensor = (filtfilt(filt.bhigh, filt.ahigh, signal_sensor1'))';
-signal_sensor = signal_sensor / norm(signal_sensor, 'fro');
-
-%reshape
-signal_sensor = reshape(signal_sensor,[],size(signal_sensor,2)/n_trials,n_trials);
-[n_sensors, l_epoch, n_trials] = size(signal_sensor);
-
-t.signal = toc;
+if params.ip==9 % source localization is varied
+    %reload data from ip1 to keep them constant and only vary the source
+    %localization
+    params_save = params;
+    load(sprintf('%s/pac_sensorsig/%d.mat',DIROUT1,params.iit));
+    params = params_save;
+    clear params_save
+else
+    %% signal generation
+    tic
+    if params.ip == 3 %snr/ noise mix is varied
+        %reload data from ip1 to keep them constant and only vary the snr
+        %or noise mix
+        params_save = params;
+        load(sprintf('%s/pac_sig/%d.mat',DIROUT1,params.iit));
+        params= params_save;
+        clear params_save
+    else
+        
+        % getting atlas, voxel and roi indices; active voxel of each region
+        % is aleady selected here
+        fprintf('Getting atlas positions... \n')
+        D = fp_get_Desikan(params.iReg);
+        
+        %signal generation
+        fprintf('Signal generation... \n')
+        [sig,brain_noise,sensor_noise,L,iroi_phase, iroi_amplt,D, fres, n_trials,filt] = ...
+            fp_pac_signal(params,D);
+        
+        if params.ip==1 %if ip1, save sig for ip3
+            dir1 =  sprintf('%s/pac_sig/',DIROUT1);
+            if ~exist(dir1); mkdir(dir1); end
+            outname = sprintf('%s/pac_sig/%d.mat',DIROUT1,params.iit);
+            save(outname,'-v7.3')
+        end
+        
+    end
+    
+    %combine noise sources
+    noise = params.iss*brain_noise + (1-params.iss)*sensor_noise;
+    noise = noise ./ norm(noise(:),'fro');
+    %combine signal and noise
+    signal_sensor1 = params.isnr*sig + (1-params.isnr)*noise;
+    signal_sensor1 = signal_sensor1 ./ norm(signal_sensor1(:), 'fro');
+    
+    %high-pass signal
+    signal_sensor = (filtfilt(filt.bhigh, filt.ahigh, signal_sensor1'))';
+    signal_sensor = signal_sensor / norm(signal_sensor, 'fro');
+    
+    %reshape
+    signal_sensor = reshape(signal_sensor,[],size(signal_sensor,2)/n_trials,n_trials);
+    [n_sensors, l_epoch, n_trials] = size(signal_sensor);
+    
+    t.signal = toc;
+    
+    if params.ip==1 %if ip1, save sig for ip9
+        dir1 =  sprintf('%s/pac_sensorsig/',DIROUT1);
+        if ~exist(dir1); mkdir(dir1); end
+        outname = sprintf('%s/pac_sensorsig/%d.mat',DIROUT1,params.iit);
+        save(outname,'-v7.3')
+    end
+end
 
 %% Leadfield
 
@@ -42,7 +76,7 @@ t.signal = toc;
 L_backward = L(:, D.ind_cortex, :);
 
 %% null distribution for shabazi method
-if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6
+if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6 || params.ip == 9 || params.case == 1
     tic
     [W,~] = runica(signal_sensor(:,:));
     
@@ -51,11 +85,17 @@ if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6
     
     for ishuf = 1:params.nshuf
         %shuffling
-        fprintf(['Shuffle '  num2str(ishuf) '\n'])        
+        fprintf(['Shuffle '  num2str(ishuf) '\n'])
         signal_shuf = fp_shuffle_shab(W,signal_unmixed);
         
         %lcmv
-        A = fp_get_lcmv(signal_shuf,L_backward);
+        if strcmp(params.filt,'lf')
+            A = fp_get_lcmv_filtered(signal_shuf,L_backward,filt);           
+        elseif strcmp(params.filt,'l')
+            A = fp_get_lcmv(signal_shuf,L_backward);
+        else
+            error('wrong filter parameter')
+        end
         
         %dimesionality reduction
         signal_roi_shuf = fp_dimred(signal_shuf,D,A,params.t);
@@ -68,10 +108,16 @@ if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6
     t.shab = toc;
 end
 
-%% 
+%%
 
 %lcmv
-A = fp_get_lcmv(signal_sensor,L_backward);
+if strcmp(params.filt,'lf')
+    A = fp_get_lcmv_filtered(signal_sensor,L_backward,filt);
+elseif strcmp(params.filt,'l')
+    A = fp_get_lcmv(signal_sensor,L_backward);
+else
+    error('wrong filter parameter')
+end
 
 %Dimensionality reduction
 signal_roi = fp_dimred(signal_sensor,D,A,params.t);
@@ -82,15 +128,16 @@ if params.case == 1
     % bispectra
     fprintf(['Calculating bispectra \n'])
     
-    nshuf = 100; 
+    nshuf = 100;
     tic
     [b_orig, b_anti] = fp_pac_bispec_uni(signal_roi,fres,filt,nshuf);
     t.bispec = toc;
     
-    for proi = 1:nroi 
-        for aroi = 1:nroi 
+    for proi = 1:nroi
+        for aroi = 1:nroi
             p_orig(proi,aroi) = sum(squeeze(b_orig(proi,aroi,1))<squeeze(b_orig(proi,aroi,2:end)))/nshuf;
             p_anti(proi,aroi) = sum(squeeze(b_anti(proi,aroi,1))<squeeze(b_anti(proi,aroi,2:end)))/nshuf;
+            
         end
     end
 else
@@ -110,8 +157,8 @@ else
     t.ortho = toc;
     
     %shabazi
-    if params.ip == 1
-        pac_shabazi = (pac_standard-mean(pac_shuf,3))/std(pac_shuf,[],3);
+    if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6 || params.ip == 9
+        pac_shahbazi = (pac_standard-mean(pac_shuf,3))/std(pac_shuf,[],3);
     end
     
     % bispectra
@@ -130,28 +177,27 @@ else
         iroi_phase(1:params.iInt(1))=[];
     end
     
-    pr_shabazi=[];
-    if params.ip==1
-        [pr_shabazi] = fp_pr_pac(pac_shabazi,iroi_amplt,iroi_phase);
+    pr_shahbazi=[];
+    if params.ip == 1 || params.ip==4 || params.ip == 5 || params.ip == 6 || params.ip == 9
+        [pr_shahbazi] = fp_pr_pac(pac_shahbazi,iroi_amplt,iroi_phase);
     end
     [pr_standard] = fp_pr_pac(pac_standard,iroi_amplt,iroi_phase);
     [pr_ortho] = fp_pr_pac(pac_ortho,iroi_amplt,iroi_phase);
     [pr_bispec_o] = fp_pr_pac(b_orig,iroi_amplt,iroi_phase);
     [pr_bispec_a] = fp_pr_pac(b_anti,iroi_amplt,iroi_phase);
     [pr_bispec_o_norm] = fp_pr_pac(b_orig_norm,iroi_amplt,iroi_phase);
-    [pr_bispec_a_norm] = fp_pr_pac(b_anti_norm,iroi_amplt,iroi_phase);
-    
+    [pr_bispec_a_norm] = fp_pr_pac(b_anti_norm,iroi_amplt,iroi_phase);    
     
     %save only evaluation parameters
     outname1 = sprintf('%spr_%s.mat',DIROUT,params.logname);
     save(outname1,...
-        'pr_standard','pr_ortho','pr_shabazi','pr_bispec_o','pr_bispec_a',...
+        'pr_standard','pr_ortho','pr_shahbazi','pr_bispec_o','pr_bispec_a',...
         'pr_bispec_o_norm','pr_bispec_a_norm','t',...
         '-v7.3')
     
 end
 
-%% Saving workspace 
+%% Saving workspace
 
 fprintf('Saving... \n')
 %save all
