986bcc3f43c5ed580b19e1ff3a9f1ce9595b3060
fpellegrini <franziskapellegrini@web.de>
2022-05-25 11:59:26 +0200

    small change

--- fp_submit_pacsim.m
+++ fp_submit_pacsim.m
@@ -3,7 +3,7 @@ fp_addpath_pac
 cd ~/matlab/fp/PAC/
 nit = 100;
 
-for ip = [1]
+for ip = [4:5] %1 4 5
     mgsub({},@fp_eval_pac_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*3)
 end
--- pac_init.m
+++ pac_init.m
@@ -1,6 +1,6 @@
 
 params. iInt = [1]; 
-params.case = 2; 
+params.case = 1; 
 params.isnr=0.7; 
 params.t=0;
 
@@ -8,7 +8,9 @@ params.iReg=1;
 params.iss = 0.9; 
 params.ifilt = 'l';
 params.pips = 1;
-params.nshuf = 100; 
+params.nshuf = 3; 
+params.ip = 2;
+params.filt = 'l';
 %%
 % addpath(genpath('~/Dropbox/Franziska/MEG_Project/matlab/libs/fieldtrip-20211001/external/fastica'))
 addpath('~/Dropbox/Franziska/PAC_AAC_estimation/matlab/fp_pac/')
