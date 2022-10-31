f5dba2d2729c20a503073302624b898382cb9aac
fpellegrini <franziskapellegrini@web.de>
2022-10-31 13:47:55 +0100

    .

--- fp_submit_pac_rde.m
+++ fp_submit_pac_rde.m
@@ -1,6 +1,6 @@
 cd ~/matlab/fp/PAC/
 fp_addpath_pac
-cd ~/matlab/fp/PAC/RDE/
+cd ~/matlab/fp/PAC/
 nsubs = 26;
 
 mgsub({},@fp_pac_test_clus,{},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nsubs) ]) %' -q 2jobs'
