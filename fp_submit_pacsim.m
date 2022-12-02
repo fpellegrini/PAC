a5a2fe641ba3b04776785b400b3c2ded580001a9
fpellegrini <franziskapellegrini@web.de>
2022-12-02 11:25:57 +0100

    sim4 with canolty metric and controlled rng

--- fp_submit_pacsim.m
+++ fp_submit_pacsim.m
@@ -3,7 +3,7 @@ fp_addpath_pac
 cd ~/matlab/fp/PAC/
 nit = 100;
 
-for ip = [10:13]
+for ip = [1] %1
     mgsub({},@fp_eval_pac_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*3)
 end
