938751394405527f94b1bce171729e98d24a429c
fpellegrini <franziskapellegrini@web.de>
2023-02-02 16:28:33 +0100

    minor changes

--- fp_submit_2chan_sim.m
+++ fp_submit_2chan_sim.m
@@ -3,4 +3,4 @@ fp_addpath_pac
 cd ~/matlab/fp/PAC/
 nit = 100;
 
-mgsub({},@fp_2chan_sim,{},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ])
+mgsub({},@fp_2chan_sim,{},'qsub_opts',['-l h_vmem=4G -t 1-' num2str(nit) ])
--- fp_submit_pacsim.m
+++ fp_submit_pacsim.m
@@ -4,7 +4,7 @@ cd ~/matlab/fp/PAC/
 nit = 100;
 varIp = [1 2 3 10 11 12 13];
 
-for ip = [1 2 3 10 11 12 13]
+for ip = [1 2 3 10]
     mgsub({},@fp_eval_pac_sim,{ip},'qsub_opts',['-l h_vmem=16G -t 1-' num2str(nit) ]) %' -q 2jobs'
     pause(60*2)
 end
