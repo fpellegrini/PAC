f6c1dae334f5db2ca6d8bbff09d4fada05b626de
fpellegrini <franziskapellegrini@web.de>
2023-02-15 12:40:19 +0100

    calculate univar baseline

--- fp_submit_2chan_sim.m
+++ fp_submit_2chan_sim.m
@@ -3,4 +3,5 @@ fp_addpath_pac
 cd ~/matlab/fp/PAC/
 nit = 16;
 
-mgsub({},@fp_2chan_sim,{},'qsub_opts',['-l h_vmem=4G -t 1-' num2str(nit) ])
+% mgsub({},@fp_2chan_sim,{},'qsub_opts',['-l h_vmem=4G -t 1-' num2str(nit) ])
+mgsub({},@fp_2chan_sim_baseline,{},'qsub_opts',['-l h_vmem=4G -t 1-' num2str(nit) ])
