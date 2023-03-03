83cc3d9081a6a1a961b9966b7b89024b19ff6eac
fpellegrini <franziskapellegrini@web.de>
2023-03-03 10:55:48 +0100

    minor changes

--- fp_stouffer.m
+++ fp_stouffer.m
@@ -2,7 +2,14 @@ function p_stouff = fp_stouffer(p)
 
 nsub = numel(p);
 p(p==1)=0.9999;
-p(p==0)=0.00001; 
+p(p==0)=0.001; 
 zsr= norminv(p);
 zr = squeeze(sum(zsr,2)./sqrt(nsub));
-p_stouff = normpdf(zr);
+
+%ensure that disproportionally high p-values don't lead to low p-values
+%after stouffer
+if (zr>0)
+    p_stouff = 1;
+else
+    p_stouff = normpdf(zr);
+end
